4364e330dd6943f7102285ec7d3b4d59a0f536c9
charrb12304 <charrbrennan@gmail.com>
2025-03-02 19:34:17 -0600

    continued follow along 1

---
+++ CGEProject1/Assets/Scripts/TankPlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankPlayerController : MonoBehaviour
+{
+    public float speed;
+
+    public float turnSpeed;
+
+    public float horizontalInput;
+
+    public float verticalInput;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Move forward
+        //transform.Translate(0,1,0);
+
+        //transform.Translate(Vector2.right);
+
+      //transform.Translate(Vector2.right * Time.deltaTime * speed);
+
+        //Get Input from the User - Do this in Update()
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //have player forward with vertical input
+        transform.Translate(Vector2.right * Time.deltaTime * speed * verticalInput);
+
+        //Rotate the player with horizontal input
+        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
